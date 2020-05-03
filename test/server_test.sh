faae6ad241c682c00389e0a9f0b297ae69163cea
shira <shira7867@gmail.com>
2020-05-03 22:33:00 +0900

    test

---
+++ test/server_test.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+cd "$(dirname $0)"
+
+SERVER_URL=http://192.168.0.102:3000/control
+
+echo "28C Auto On"
+curl -X POST -H "Content-Type:application/json" -d '{"switch":"on", "mode": "cool"}' "$SERVER_URL"
+echo ""
+sleep 10
+
+echo "22H Auto On"
+curl -X POST -H "Content-Type:application/json" -d '{"switch":"on", "mode": "heat"}' "$SERVER_URL"
+echo ""
+sleep 10
+
+echo "Off"
+curl -X POST -H "Content-Type:application/json" -d '{"switch":"off"}' "$SERVER_URL"
+echo ""
